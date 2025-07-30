a49751688de741e57cdd3dcc0ce7d81447c9214c
Raushan Kumar <raushankumar.raus6202@gmail.com>
2025-07-30 14:10:42 +0530

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter } from 'rxjs/operators';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -20,4 +23,17 @@ import { FooterComponent } from './components/footer/footer.component';
     }
   `]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      gtag('event', 'page_view', {
+        page_path: event.urlAfterRedirects,
+        page_location: window.location.href
+      });
+    });
+  }
+}
